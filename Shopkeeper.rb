bb9aa02de8bbe84e983edf59c977defdfc2668e2
padmaja <padmajal@shriffle.com>
2021-09-21 15:22:57 +0530

    item-listing

--- Shopkeeper.rb
+++ Shopkeeper.rb
@@ -1,7 +1,9 @@
 #!usr/bin/ruby -w
       
 module List 
-    $temp = " "
+    $temp = Array.new()
+    $val  = Array.new()
+    $i = $j = 0
     $hash_ = {"salt"=>40, "pepper"=>55, "taco"=>120, "honey"=>70, "jelly"=>87, "ketchup"=>80, 
              "butter"=>140, "pickles"=>125, "sugar"=>45, "soap"=>30, "cleaner"=>98, "tea"=>90 }   
 
@@ -33,18 +35,24 @@ module Option
         when 2
             puts "Enter name of item :"
             $hash_.each do |x,y|  
-            input = gets.chomp  
-                if $hash_.key?(input)
-                    if input.empty? == false
-                        $temp = $temp + input + "\t" 
-                    else   
-                        break
-                    end
+            input = gets.chomp 
+                if input.empty? == false
+                       $temp[$i] = "#{input}"
+                       $i = $i + 1
+                       $val[$j] = $hash_[input]
+                       $j = $j + 1
                 else
                     break     
                 end
-            end          
-            puts "List is : #{$temp}"
+            end    
+            
+            puts " Name \t Price \n "
+            name1 = $temp.each
+            name2 = $val.each
+            loop do 
+                n1,n2 = name1.next,name2.next
+                puts " #{n1}   #{n2}  "
+            end
 
         when 3
             print "Enter name of item you want to delete : "
